90bdc544c3deb6f3f13bcdd795d406bd63228ccc
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2023-02-07 17:32:07 -0500

    Usuario
    CRUD usuario

---
+++ source/components/index.ts
@@ -0,0 +1,12 @@
+
+
+/* Acceso para las capertas*/
+
+export { default as usuarioRouter } from "./usuario";
+export { default as  restauranteRouter} from "./restaurante";
+export { default as comentarioRouter } from "./comentarios";
+export { default as platoRouter } from "./platos";
+
+/* una ves terminado el CRUD poner igual a lo de arriba */
+
+
